d645489609cc5d79656960c4ced3317c91090c5a
PranamPatro <pranampatro93@gmail.com>
2023-09-05 11:47:13 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import "./style.css";
+import logo from  './../public/assets/logo.svg'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{ width:"100%", height:"100vh", display:"flex", flexDirection:"column"
+    }}>
+      <div style={{flex:1, display:"flex"}}>
+        <div style={{flex:1, backgroundColor:"yellow", display:"flex", justifyContent:"start", alignItems:"center"}}><img src={logo}/></div>
+        <div style={{flex:3, backgroundColor:"grey", display:"flex", justifyContent:"center", alignItems:"center"}}>menus </div>
+        <div style={{flex:1, display:"flex", justifyContent:"end", alignItems:"center"}}>logo 1</div>
+      </div>
+      <div style={{flex:9, display:"flex"}}>
+        <div style={{backgroundColor:"cyan", flex:1}}>sub menu</div>
+        <div style={{backgroundColor:"green", flex:4}}>main contnet</div>
+      </div>
     </div>
   );
 }
